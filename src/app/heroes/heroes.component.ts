84ce6b6334c75e6cce4e09e0839e9061e1bd54f9
steph <steph@fr>
2019-08-30 17:27:15 +0200

    1er commit sur le tuto angular, arrive apeine sur les observable

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: "Windstorn"
+  // }
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+ 
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+
+  // getHeroes(): void
+  // {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+      getHeroes(): void {
+        this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
+      }
+
+
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+}
